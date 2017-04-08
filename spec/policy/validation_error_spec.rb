8d58f806164fd49c75a2e3abc7fe77075aff1f45
JewelSam <mrswlasowa@gmail.com>
2017-04-08 20:51:54 +0300

    Use #let instead of #before

--- spec/policy/validation_error_spec.rb
+++ spec/policy/validation_error_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe Tram::Policy::ValidationError do
-  before(:all) do
-    @policy = Tram::Policy.new(Article.new)
-    @error = Tram::Policy::ValidationError.new(@policy, @policy.errors)
-  end
+  let(:policy) { Tram::Policy.new(Article.new) }
+  subject(:error) { Tram::Policy::ValidationError.new(policy, policy.errors) }
 
   it { expect(described_class).to be < RuntimeError }
 
   context '.policy' do
     it 'should return policy' do
-      expect(@error.policy).to eq(@policy)
+      expect(error.policy).to eq(policy)
     end
   end
 end
